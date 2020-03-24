df10d002614345565c983dbb2ae0ecff64e0cd5d
Kyle <kylepcole@gmail.com>
2020-03-24 10:26:37 -0400

    homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent as Home } from './components/home/home.component'
+import { BreweriesComponent as Breweries } from './components/breweries/breweries.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: Home },
+	{ path: 'breweries', component: Breweries },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
